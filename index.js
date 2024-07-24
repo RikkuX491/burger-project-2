ca2d9af813a73d14da92ab2e1223fd8909db72dc
Ricardo Guerra <RikkuG491@gmail.com>
2024-07-24 13:34:48 -0400

    Wrote code to add burger data to the web page

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
+const burgersList = document.getElementById('burgers-list')
+
 fetch("http://localhost:3000/burgers")
 .then(response => response.json())
 .then(burgers => {
-    console.log(burgers)
+    burgers.forEach(addBurgerToBurgersList)
 })
+
+function addBurgerToBurgersList(burger){
+    const liElement = document.createElement('li')
+
+    const idElement = document.createElement('h3')
+    idElement.textContent = `Burger # ${burger.id}`
+    
+    const nameElement = document.createElement('h3')
+    nameElement.textContent = `Name: ${burger.name}`
+    
+    const imageElement = document.createElement('img')
+    imageElement.src = burger.image
+
+    const priceElement = document.createElement('h3')
+    priceElement.textContent = `Price: ${burger.price}`
+    
+    liElement.appendChild(idElement)
+    liElement.appendChild(nameElement)
+    liElement.appendChild(imageElement)
+    liElement.appendChild(priceElement)
+    burgersList.appendChild(liElement)
+}
