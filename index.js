595a89803fe00d05e6ff7a3f7ed89cd5218adc13
Ricardo Guerra <RikkuG491@gmail.com>
2024-07-24 14:00:53 -0400

    Modified code in addBurgerToBurgersList() function in index.js and changed <ul> element to a <div> element in index.html

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const burgersList = document.getElementById('burgers-list')
+const burgerMenu = document.getElementById('burger-menu')
 
 fetch("http://localhost:3000/burgers")
 .then(response => response.json())
@@ -7,28 +7,13 @@ fetch("http://localhost:3000/burgers")
 })
 
 function addBurgerToBurgersList(burger){
-    const liElement = document.createElement('li')
-
-    const idElement = document.createElement('h3')
-    idElement.textContent = `Burger # ${burger.id}`
-    
-    const nameElement = document.createElement('h3')
-    nameElement.textContent = `Name: ${burger.name}`
-    
     const imageElement = document.createElement('img')
     imageElement.src = burger.image
     imageElement.addEventListener('click', () => {
         displayBurgerDetails(burger)
     })
-
-    const priceElement = document.createElement('h3')
-    priceElement.textContent = `Price: ${burger.price}`
     
-    liElement.appendChild(idElement)
-    liElement.appendChild(nameElement)
-    liElement.appendChild(imageElement)
-    liElement.appendChild(priceElement)
-    burgersList.appendChild(liElement)
+    burgerMenu.appendChild(imageElement)
 }
 
 function displayBurgerDetails(burger){
